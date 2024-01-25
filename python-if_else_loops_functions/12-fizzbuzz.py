240ff79975a060a4eaa18107e411558e4dd97332
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-25 14:56:59 +0100

    Update Task 12 - added docstring

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    """
+    prints the numbers from 1 to 100 separated by a space
+
+    """
     for i in range(1, 100):
         if i % 3 == 0:
             print("Fizz", end=" ")
@@ -7,3 +11,4 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
+
