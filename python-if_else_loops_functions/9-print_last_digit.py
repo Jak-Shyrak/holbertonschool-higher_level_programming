a30a32aae3f9e849481f963e9213b328a0b5c947
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-25 15:28:11 +0100

    Update Task 9 - Add docstring

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    """
+    Prints the last digit of a number
+
+    Parameters:
+    - number (int): Number whose the last digitmust be returned
+
+    Returns:
+    int: Value of the last digit
+
+    """
     l_digit = abs(number) % 10
     print(l_digit, end='')
     return(l_digit)
