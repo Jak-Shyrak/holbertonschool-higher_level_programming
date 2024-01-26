57d311d2313f840c1f86ddfb7150c600069dc448
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-26 01:10:08 +0100

    Update Task 10

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def add(a, b):
-    """
-    Adds two integers and returns the result.
-
-    Parameters:
-    - a (int): First integer to add
-    - b (int): Seconde integer to add
-
-    Returns:
-    int: the value of a + b
-
-    """
-    
     return (a + b)
