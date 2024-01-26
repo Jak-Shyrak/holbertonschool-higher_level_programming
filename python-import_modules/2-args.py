6dfcf48953b76604851f85df23d12fd949e14c57
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-26 12:57:39 +0100

    Update task 2 - Adding a ":" at arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 from sys import argv
 
 if __name__ == '__main__':
-    print(len(argv) - 1, "arguments")
+    print(len(argv) - 1, "arguments:")
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
