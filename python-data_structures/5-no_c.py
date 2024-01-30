bc0219f3e20c2177483af4d5c3b40ff4f6cfe8c5
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-30 20:58:28 +0100

    Update task 5 - 7 - (removing  comments)

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,12 +2,8 @@
 def no_c(my_string):
     new_string = ""
 
-    # Iterate through each character in the input string
     for c in my_string:
-        # Check if the current character is not 'c' or 'C'
         if c != 'c' and c != 'C':
-            # If not 'c' or 'C', add it to the result string
             new_string += c
 
-    # Return the final result string
     return new_string
--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two integers from each tuple or use 0 if not present
     a = tuple_a[:2] + (0, 0)
     b = tuple_b[:2] + (0, 0)
 
-    # Calculate the sum of corresponding elements
     result = (a[0] + b[0], a[1] + b[1])
 
     return result
