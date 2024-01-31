9e531a83ac9d865459ca4ca897355bd2fe1e82ca
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-31 18:18:26 +0100

    Update Task 2 - Tested Locally : OK (missing she bang )

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
+    # Use a set to store unique integers
     unique_set = set(my_list)
-    return sum(unique_set)
+
+    # Sum the unique integers in the set and return the result
+    result = sum(unique_set)
+    return result
