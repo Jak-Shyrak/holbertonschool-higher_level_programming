3a12c50dfded2108150a374564e8526749c31914
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-31 18:15:34 +0100

    Update Task 2 - Tested Locally : OK (removing condition)

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,6 @@
 def uniq_add(my_list=[]):
     # Use a set to store unique integers
-    unique_set = set()
-
-    # Iterate through the list and add unique integers to the set
-    for i in my_list:
-        unique_set.add(i)
-
+    unique_set = set(my_list)
     # Sum the unique integers in the set and return the result
     result = sum(unique_set)
     return result
