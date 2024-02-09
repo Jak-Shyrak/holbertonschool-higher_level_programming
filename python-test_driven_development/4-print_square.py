4d77562ba62a645393d6ba8678be6ddf2ef60928
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-02-09 17:19:48 +0100

    Update task 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+Prints a square with the character #
+"""
+
+
 def print_square(size):
     """
     Prints a square with the character #
     """
-
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if isinstance(size, float) and size < 0:
@@ -11,5 +15,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for _ in range(size):
-        print("#" * size)
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
