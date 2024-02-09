1b42b94446be29234c2bcd96b387694b77fe9342
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-02-09 17:12:26 +0100

    Update Task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
+"""
+Divides all elements of a matrix
+
+
+"""
+
+
 def matrix_divided(matrix, div):
     """
-    Divides all elements of a matrix.
+    divide a matrix by div
     """
-
-    if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix\
-                        (list of lists) of integers/floats")
-
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError("matrix must be a matrix\
-                             (list of lists) of integers/floats")
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-
-    if type(div) is not int and type(div) is not float:
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
-    return new_matrix
+    new_matrix = []
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for value in row:
+            msg = "matrix must be a matrix (list of lists) of integers/floats"
+            if not isinstance(value, (int, float)):
+                raise TypeError(msg)
+            new_row.append(round(value / div, 2))
+        new_matrix.append(new_row)
+
+    return (new_matrix)
