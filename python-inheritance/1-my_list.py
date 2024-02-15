467aa30859fcae28a412f86c7a0bb1fbce8f6227
Jak Shyrak <bourhlef.yacine53@gmail.com>
2024-02-15 12:13:34 +0100

    Task 1 - Tested Locally : OK

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    class MyList that inherits from list
+"""
+
+class MyList(list):
+    
+    def print_sorted(self):
+            """
+            Prints the list, but sorted (ascending sort)
+            """
+            for element in self:
+                if type(element) is not int:
+                    raise TypeError("all the elements of the list will be int")
+            print(sorted(self))
