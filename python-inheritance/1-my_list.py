4c2219a0e01248c2f4fcf0b08d4af51833708c06
Jak Shyrak <bourhlef.yacine53@gmail.com>
2024-02-15 12:17:19 +0100

    Update Task 1 (pycodestyle requirement)

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,13 +3,16 @@
     class MyList that inherits from list
 """
 
+
 class MyList(list):
-    
+    """
+    Class MyList that inherits from list
+    """
     def print_sorted(self):
-            """
-            Prints the list, but sorted (ascending sort)
-            """
-            for element in self:
-                if type(element) is not int:
-                    raise TypeError("all the elements of the list will be int")
-            print(sorted(self))
+        """
+        Prints the list, but sorted (ascending sort)
+        """
+        for element in self:
+            if type(element) is not int:
+                raise TypeError("type should be integer")
+        print(sorted(self))
